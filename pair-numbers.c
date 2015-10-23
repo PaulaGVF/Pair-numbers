ec95b889604d51656f8189ae14762eafb9bd163c
Paula Galindo <paulagalindovf@live.com>
2015-10-23 20:41:59 -0200

    Create pair-numbers.c

---
+++ pair-numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{int a;
+a=0;
+while(a<=50)
+            {printf("%d\n",a);
+             a=a+2;
+             }
+
+system("PAUSE");
+    return 0;
+}
